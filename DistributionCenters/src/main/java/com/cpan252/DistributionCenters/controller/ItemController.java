790687857b95a8fc2c78db41db6b524e66bdf978
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-04-12 17:42:20 -0400

    changes

--- DistributionCenters/src/main/java/com/cpan252/DistributionCenters/controller/ItemController.java
+++ DistributionCenters/src/main/java/com/cpan252/DistributionCenters/controller/ItemController.java
@@ -8,13 +8,8 @@ import com.cpan252.DistributionCenters.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -48,5 +43,11 @@ public class ItemController {
         model.addAttribute("itemDTO", new ItemDTO()); // change "ItemDTO" to "itemDTO"
     }
 
+    @PostMapping
+    public String searchItemBrandAndName(@ModelAttribute ItemDTO itemDTO, Model model, @RequestParam("brand") String brand) {
+        model.addAttribute("items", itemRepository.findByBrandFromAndName(Item.Brand.valueOf(brand),
+                itemDTO.getName()));
+        return "items";
+    }
 
 }
